f64cc12e45efbc521d089913521856e18b16e63e
watanabeyu <fight.ippatu5648@gmail.com>
2024-10-02 14:19:15 +0900

    fix

--- apps/compare-like-tailwind/tailwind.config.js
+++ apps/compare-like-tailwind/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["src/app/**/*.{js,jsx,ts,tsx}"],
+  content: ["src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   plugins: [],
 };
